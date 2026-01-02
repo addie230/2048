155e4f332edbb033b984dccc1942a0ca7bd88c0c
addie230 <adrianamaslarova23@gmail.com>
2026-01-03 00:14:55 +0200

    add constants

---
+++ constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const int MIN_N = 4;
+const int MAX_N = 10;
+
+const int MAX_BOARD = 10;
+
+const int MAX_NAME = 100;
+const int TOP_K = 5;
+
+#endif // CONSTANTS_H
