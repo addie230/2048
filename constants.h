d4ddbedcc74186c4908fd7c8ce2a53e9e2433180
addie230 <adrianamaslarova23@gmail.com>
2026-01-03 00:31:12 +0200

    update constants

--- constants.h
+++ constants.h
@@ -1,12 +1,13 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const int MIN_N = 4;
-const int MAX_N = 10;
+const int MIN_BOARD_SIZE = 4;
+const int MAX_BOARD_SIZE = 10;
 
-const int MAX_BOARD = 10;
+const int MAX_NAME_LEN = 100;
+const int MAX_LEADERBOARD_ENTRIES = 5;
 
-const int MAX_NAME = 100;
-const int TOP_K = 5;
+const int MAX_FILENAME_LEN = 64;
+const int MAX_COMMAND_LEN = 16;
 
 #endif // CONSTANTS_H
