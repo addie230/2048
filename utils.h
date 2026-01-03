67a5315a2de15693b8970024f0a05272accb8512
addie230 <adrianamaslarova23@gmail.com>
2026-01-03 20:13:30 +0200

    declare string utility functions

--- utils.h
+++ utils.h
@@ -1,1 +1,11 @@
-#pragma once
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "constants.h"
+
+int myStrlen(const char* str);
+void myStrcpy(char* dest, const char* src);
+void myStrcat(char* dest, const char* src);
+int myStrcmp(const char* a, const char* b);
+
+#endif // UTILS_H
