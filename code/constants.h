d7a807f063432fe4f13c06a9bbc999333ade1efb
addie230 <adrianamaslarova23@gmail.com>
2026-01-15 23:23:51 +0200

    add more constants

--- code/constants.h
+++ code/constants.h
@@ -7,4 +7,18 @@ const char CMD_DOWN = 's';
 const char CMD_RIGHT = 'd';
 const char CMD_QUIT = 'q';
 
+const unsigned MIN_DIM = 4;
+const unsigned MAX_DIM = 10;
+const unsigned MAX_INPUT_LINE = 1024;
+
+const unsigned MAX_NICKNAME_LEN = 101;
+const unsigned TOP_RESULTS = 5;
+
+const unsigned TILE_2 = 2;
+const unsigned TILE_4 = 4;
+const unsigned TILE_8 = 8;
+
+const unsigned EARLY_GAME_MAX_TILE = 128; //early game limit
+const unsigned MID_GAME_MAX_TILE = 512; //late game limit
+
 #endif
