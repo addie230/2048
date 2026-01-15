1ed1804c5cbe4973dd6542dcdfc2c29c35ef500f
addie230 <adrianamaslarova23@gmail.com>
2026-01-15 21:48:47 +0200

    add command constants

---
+++ code/constants.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const char CMD_UP = 'w';
+const char CMD_LEFT = 'a';
+const char CMD_DOWN = 's';
+const char CMD_RIGHT = 'd';
+const char CMD_QUIT = 'q';
+
+#endif
