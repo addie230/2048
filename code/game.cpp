25d461d1fe54a6a8bbeee3bf1188e2c9df51cc48
addie230 <adrianamaslarova23@gmail.com>
2026-01-16 14:55:26 +0200

    add random tile spawn after moves

--- code/game.cpp
+++ code/game.cpp
@@ -1,5 +1,6 @@
 #include "game.h"
 #include "utils.h"
+#include <cstdlib>
 
 void initBoard(unsigned board[MAX_DIM][MAX_DIM], unsigned dim) {
 	for (unsigned r = 0; r < dim; r++) {
@@ -29,4 +30,37 @@ unsigned sumBoard(const unsigned board[MAX_DIM][MAX_DIM], unsigned dim) {
 		}
 	}
 	return sum;
+}
+
+unsigned countEmpty(const unsigned board[MAX_DIM][MAX_DIM], unsigned dim) {
+	unsigned cnt = 0;
+	for (unsigned r = 0; r < dim; r++) {
+		for (unsigned c = 0; c < dim; c++) {
+			if (board[r][c] == 0) {
+				cnt++;
+			}
+		}
+	}
+	return cnt;
+}
+
+void addRandomTile(unsigned board[MAX_DIM][MAX_DIM], unsigned dim) {
+	unsigned empty = countEmpty(board, dim);
+	if (empty == 0) {
+		return;
+	}
+	unsigned pick = (unsigned)(rand() % (int)empty);
+	unsigned seen = 0;
+	for (unsigned r = 0; r < dim; r++) {
+		for (unsigned c = 0; c < dim; c++) {
+			if (board[r][c] == 0) {
+				if (seen == pick) {
+					unsigned m = maxTile(board, dim);
+					board[r][c] = randomTileValue(m);
+					return;
+				}
+				seen++;
+			}
+		}
+	}
 }
--- code/game.h
+++ code/game.h
@@ -7,4 +7,7 @@ void initBoard(unsigned board[MAX_DIM][MAX_DIM], unsigned dim);
 unsigned maxTile(const unsigned board[MAX_DIM][MAX_DIM], unsigned dim);
 unsigned sumBoard(const unsigned board[MAX_DIM][MAX_DIM], unsigned dim);
 
+unsigned countEmpty(const unsigned board[MAX_DIM][MAX_DIM], unsigned dim);
+void addRandomTile(unsigned board[MAX_DIM][MAX_DIM], unsigned dim);
+
 #endif
