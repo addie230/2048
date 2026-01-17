c9ae76039e005bd7e29c187f101cbbb58ed7f47e
addie230 <adrianamaslarova23@gmail.com>
2026-01-17 13:22:18 +0200

    add nickname and board size input validation

--- code/ui.cpp
+++ code/ui.cpp
@@ -26,3 +26,44 @@ static void printMenu() {
 	cout << "3. Exit" << endl;
 	cout << "Choice: ";
 }
+
+static bool isAllSpaces(const char* s) {
+	if (!s) return true;
+	for (unsigned i = 0; s[i] != '\0'; i++) {
+		if (s[i] != ' ' && s[i] != '\t') {
+			return false;
+		}
+	}
+	return true;
+}
+
+static void readNickname(char nickname[MAX_NICKNAME_LEN]) {
+	while (true) {
+		cout << "Enter nickname (max 100 chars): ";
+		cin.getline(nickname, MAX_NICKNAME_LEN);
+		if (cin.fail()) {
+			cin.clear();
+			clearInputLine();
+			cout << "Nickname too long. Try again." << endl;
+			continue;
+		}
+		if (nickname[0] == '\0' || isAllSpaces(nickname)) {
+			cout << "Invalid nickname. Try again." << endl;
+			continue;
+		}
+		return;
+	}
+}
+
+static unsigned readDimension() {
+	unsigned dim = 0;
+	cout << "Enter board size (4..10): ";
+	cin >> dim;
+	while (cin.fail() || dim < MIN_DIM || dim > MAX_DIM) {
+		clearInputLine();
+		cout << "Invalid size. Enter board size (4..10): ";
+		cin >> dim;
+	}
+	clearInputLine();
+	return dim;
+}
